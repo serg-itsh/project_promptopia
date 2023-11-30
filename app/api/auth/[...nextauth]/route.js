21c3958c2df18eb50e82c954890482a84b8d9649
Sergey Shevchenko <98112335+serg-itsh@users.noreply.github.com>
2023-11-30 18:09:47 +0200

    Added new files

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -28,7 +28,7 @@ const handler = NextAuth({
     
             return session;
         },
-        async signIn({profile}) {
+        async signIn({account, profile, user, credentials}) {
             try{
                 await connectToDB();
     
